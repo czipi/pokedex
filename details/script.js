375268d1f9c5a5c460504b87e6121b95cab47795
czipi <dani@czipper.hu>
2017-04-13 10:04:44 +0200

    Added caching of details and little refactoring.

--- details/script.js
+++ details/script.js
@@ -1,6 +1,45 @@
 var pokeList = [];
 var pokemonTypes = {};
 var pokeGroups = {};
+var pokemonDetails = {};
+
+var typeColors = {
+  normal: "A8A77A",
+  fire: "EE8130",
+  water: "6390F0",
+  electric: "F7D02C",
+  grass: "7AC74C",
+  ice: "96D9D6",
+  fighting: "C22E28",
+  poison: "A33EA1",
+  ground: "E2BF65",
+  flying: "A98FF3",
+  psychic: "F95587",
+  bug: "A6B91A",
+  rock: "B6A136",
+  ghost: "735797",
+  dragon: "6F35FC",
+  dark: "705746",
+  steel: "B7B7CE",
+  fairy: "D685AD"
+}
+
+fetchTypes("https://raw.githubusercontent.com/flaki/pokedex/master/poketypes.json");
+
+function fetchTypes(url) {
+  fetch(url).then(
+    function(response) {
+      return response.json();
+    }).then(function(types){
+      addTypes(types);
+    });
+}
+
+function addTypes(types) {
+  pokemonTypes = types;
+  transformTypesToGroups();
+  fetchPokemons('https://pokeapi.co/api/v2/pokemon/?limit=150');
+}
 
 function fetchPokemons (url) {
   fetch(url).then(
@@ -11,89 +50,64 @@ function fetchPokemons (url) {
     });
 };
 
-function addPokemons(pokemons) {	
-	  pokemons.forEach(function(pokemon){	  
-		  pokeList.push(pokemon); // lista elmentése	
-	    var main = document.createElement('div');
-	    main.setAttribute('class','list-element');
-	    main.setAttribute('id', pokemon.name);
-	    // onclick event -> fetch details
-	    main.addEventListener('click', function(e) {    	
-	    	if(e.target.nodeName == "IMG") { // ha a képre kattintottunk    		
-	    		fetchPokemonDetails(e.target.parentElement.childNodes[1].innerHTML.toLowerCase());
-	    	} else { // ha a spanra kattintottunk    		
-	    		fetchPokemonDetails(e.target.innerHTML.toLowerCase());
-	    	}    	
-
-        document.getElementById('loadingScreen').style.display = "flex";
-        document.getElementById('pokemons').style.display = "none";
-	    });
-
-	    var sub = document.createElement('div');
-	    main.appendChild(sub);
-	    
-	    var img = document.createElement('img');
-	    var id = pokemon.url.match(/\/(\d+)\/$/)[1];
-	    var imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%id.png";
-      // main.style.backgroundImage = "url('"+imgUrl.replace("%id", id)+"')";
-      // console.log("url("+imgUrl.replace("%id", id)+")");
-	    img.setAttribute('src', imgUrl.replace("%id", id));
-	    sub.appendChild(img);
-	    
-      var types = pokemonTypes[pokemon.name];
-
-      if (types.length == 1) {
-        main.style.background = "#" + typeColors[types[0]];
-        console.log("1 " + "#" + typeColors[types[0]]);
-      } else if (types.length == 2) {
-        main.style.background = "linear-gradient(90deg, #"+typeColors[types[0]]+" 50%, #"+typeColors[types[1]]+" 50%)";
+function addPokemons(pokemons) {
+  pokemons.forEach(function(pokemon){
+    pokeList.push(pokemon); // lista elmentése
+    var main = document.createElement('div');
+    main.setAttribute('class','list-element');
+    main.setAttribute('id', pokemon.name);
+    // onclick event -> fetch details
+    main.addEventListener('click', function(e) {
+      if(e.target.nodeName == "IMG") { // ha a képre kattintottunk
+        fetchPokemonDetails(e.target.parentElement.childNodes[1].innerHTML.toLowerCase());
+      } else { // ha a spanra kattintottunk
+        fetchPokemonDetails(e.target.innerHTML.toLowerCase());
       }
 
-	    document.getElementById('list').appendChild(main);
+      document.getElementById('loadingScreen').style.display = "flex";
+      document.getElementById('pokemons').style.display = "none";
+    });
 
-	    var spn = document.createElement('span');
-      spn.style.opacity = '0.5';
-      spn.style.backgroundColor = 'white';
-	    var name = pokemon.name.substring(0, 1).toUpperCase() + pokemon.name.substring(1, pokemon.name.length) ;
-	    spn.textContent = name;
+    var sub = document.createElement('div');
+    main.appendChild(sub);
 
-	    sub.appendChild(spn);
-	  });
+    var img = document.createElement('img');
+    var id = pokemon.url.match(/\/(\d+)\/$/)[1];
+    var imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%id.png";
+    // main.style.backgroundImage = "url('"+imgUrl.replace("%id", id)+"')";
+    // console.log("url("+imgUrl.replace("%id", id)+")");
+    img.setAttribute('src', imgUrl.replace("%id", id));
+    sub.appendChild(img);
 
-	  console.log(pokeList);
-	  
-	  document.getElementById('loadingScreen').style.display = "none";
-	  document.getElementById('pokemons').style.display = "flex";
-	  document.getElementById('pokemons').style.flexDirection = "column";
-	}
+    var types = pokemonTypes[pokemon.name];
 
-fetchTypes("https://raw.githubusercontent.com/flaki/pokedex/master/poketypes.json");
-
-document.getElementById("details-wrapper").addEventListener("click", function(e){
-    if (e.target === this) {
-      this.style.display = "none";
-      document.getElementById('pokemons').style.display = "flex";
+    if (types.length == 1) {
+      main.style.background = "#" + typeColors[types[0]];
+    } else if (types.length == 2) {
+      main.style.background = "linear-gradient(90deg, #"+typeColors[types[0]]+" 50%, #"+typeColors[types[1]]+" 50%)";
     }
-});
 
-document.getElementById("filter").addEventListener("change", function(){
-  var searchValue = document.getElementById("search").value.toLowerCase();
-  var typeValue = this.value.toLowerCase();
+    document.getElementById('list').appendChild(main);
 
-  searchPokemons(typeValue, searchValue);
-});
+    var spn = document.createElement('span');
+    spn.style.opacity = '0.5';
+    spn.style.backgroundColor = 'white';
+    var name = pokemon.name.substring(0, 1).toUpperCase() + pokemon.name.substring(1, pokemon.name.length) ;
+    spn.textContent = name;
 
-document.getElementById("search").addEventListener("input", function(){
-  var searchValue = this.value.toLowerCase();
-  var typeValue = document.getElementById("filter").value.toLowerCase();
+    sub.appendChild(spn);
+  });
 
-  searchPokemons(typeValue, searchValue);
-});
+
+  document.getElementById('loadingScreen').style.display = "none";
+  document.getElementById('pokemons').style.display = "flex";
+  document.getElementById('pokemons').style.flexDirection = "column";
+}
 
 function searchPokemons(type, name) {
   var pokemons = document.getElementById("list").childNodes;
 
-  console.log("search " + type + " " + name);
+  console.log("Searching for '" + name + "' in type '" + (type == "" ? "all" : "type") + "'.");
 
   if (type == "" && name == "") {
 
@@ -137,76 +151,62 @@ function searchPokemons(type, name) {
   });
 }
 
-
-var pokemonDetails = {};
 function fetchPokemonDetails(pokeName) {
-	console.log('kiválasztott poke neve:' + pokeName);
-	var poke = pokeList.find(function(poke){
-		return (poke.name == pokeName);
-	});
-	console.log(poke);
-	fetch(poke.url).then(function(response) {		  
-	      return response.json();
-	    }).then(function(pokemonDetails){
-	    	console.log(pokemonDetails);
-	    	loadPokeDetails(pokemonDetails);
-	    });
+  console.log('kiválasztott poke neve:' + pokeName);
+  var poke = pokeList.find(function(poke){
+    return (poke.name == pokeName);
+  });
+
+  if (pokeName in pokemonDetails) {
+    loadPokeDetails(pokemonDetails[pokeName]);
+  } else {
+    fetch(poke.url).then(function(response) {
+      return response.json();
+    }).then(function(pokeDetails){
+         pokemonDetails[pokeName] = pokeDetails;
+         loadPokeDetails(pokeDetails);
+       });
+  }
 }
 
-function loadPokeDetails(pokemonDetails){
-    var types = pokemonTypes[pokemonDetails.name];
+function loadPokeDetails(pokeDetails){
+  var types = pokemonTypes[pokeDetails.name];
 
-    if (types.length == 1) {
-      document.getElementById("details-wrapper").style.background = "#" + typeColors[types[0]];;
-    } else if (types.length == 2) {
-      document.getElementById("details-wrapper").style.background = "linear-gradient(90deg, #"+typeColors[types[0]]+" 50%, #"+typeColors[types[1]]+" 50%)";
-    }
+  if (types.length == 1) {
+    document.getElementById("details-wrapper").style.background = "#" + typeColors[types[0]];;
+  } else if (types.length == 2) {
+    document.getElementById("details-wrapper").style.background = "linear-gradient(90deg, #"+typeColors[types[0]]+" 50%, #"+typeColors[types[1]]+" 50%)";
+  }
 
-  var name = pokemonDetails.name.substring(0, 1).toUpperCase() + pokemonDetails.name.substring(1, pokemonDetails.name.length);
+  var name = pokeDetails.name.substring(0, 1).toUpperCase() + pokeDetails.name.substring(1, pokeDetails.name.length);
 
-	document.getElementById('poke-name').innerHTML = name;
-	document.getElementById('poke-img').setAttribute('src', pokemonDetails.sprites.front_default);
-	document.getElementById('poke-stat-hp').innerHTML = pokemonDetails.stats[5].base_stat;	
-	document.getElementById('poke-stat-attack').innerHTML = pokemonDetails.stats[4].base_stat;	
-	document.getElementById('poke-stat-defense').innerHTML = pokemonDetails.stats[3].base_stat;
-	document.getElementById('poke-stat-speed').innerHTML = pokemonDetails.stats[0].base_stat;
-	document.getElementById('poke-stat-spatk').innerHTML = pokemonDetails.stats[2].base_stat;
-	document.getElementById('poke-stat-spdef').innerHTML = pokemonDetails.stats[1].base_stat;
+  document.getElementById('poke-name').innerHTML = name;
+  document.getElementById('poke-img').setAttribute('src', pokeDetails.sprites.front_default);
+  document.getElementById('poke-stat-hp').innerHTML = pokeDetails.stats[5].base_stat;
+  document.getElementById('poke-stat-attack').innerHTML = pokeDetails.stats[4].base_stat;
+  document.getElementById('poke-stat-defense').innerHTML = pokeDetails.stats[3].base_stat;
+  document.getElementById('poke-stat-speed').innerHTML = pokeDetails.stats[0].base_stat;
+  document.getElementById('poke-stat-spatk').innerHTML = pokeDetails.stats[2].base_stat;
+  document.getElementById('poke-stat-spdef').innerHTML = pokeDetails.stats[1].base_stat;
 
   document.getElementById('loadingScreen').style.display = "none";
   document.getElementById("details-wrapper").style.display='flex';
 }
 
-function fetchTypes(url) {  
-  fetch(url).then(
-    function(response) {
-      return response.json();
-    }).then(function(types){
-      addTypes(types);
-    });
-}
-
-function addTypes(types) {
-  pokemonTypes = types;
-  console.log(pokemonTypes);
-  transformTypesToGroups();
-  fetchPokemons('https://pokeapi.co/api/v2/pokemon/?limit=150');
-}
-
-function transformTypesToGroups() {		
-	for (var pokeName in pokemonTypes) {		
-		if (pokemonTypes.hasOwnProperty(pokeName)) {			
-		    var typeArr = pokemonTypes[pokeName];		    
-		    typeArr.forEach(function(type){		    	
-		    	if (pokeGroups.hasOwnProperty(type)){
-		    		pokeGroups[type].push(pokeName);
-		    	} else {
-		    		pokeGroups[type] = [];
-		    		pokeGroups[type].push(pokeName);
-		    	}		    	
-		    });
-		}
-	}
+function transformTypesToGroups() {
+  for (var pokeName in pokemonTypes) {
+    if (pokemonTypes.hasOwnProperty(pokeName)) {
+      var typeArr = pokemonTypes[pokeName];
+      typeArr.forEach(function(type){
+        if (pokeGroups.hasOwnProperty(type)){
+          pokeGroups[type].push(pokeName);
+        } else {
+          pokeGroups[type] = [];
+          pokeGroups[type].push(pokeName);
+        }
+      });
+    }
+  }
 
   var select = document.getElementById("filter");
   for (var type in pokeGroups) {
@@ -215,26 +215,29 @@ function transformTypesToGroups() {
     option.innerHTML = type + " type";
     select.appendChild(option);
   }
-	console.log(pokeGroups);
 }
 
-var typeColors = {
-  normal: "A8A77A",
-  fire: "EE8130",
-  water: "6390F0",
-  electric: "F7D02C",
-  grass: "7AC74C",
-  ice: "96D9D6",
-  fighting: "C22E28",
-  poison: "A33EA1",
-  ground: "E2BF65",
-  flying: "A98FF3",
-  psychic: "F95587",
-  bug: "A6B91A",
-  rock: "B6A136",
-  ghost: "735797",
-  dragon: "6F35FC",
-  dark: "705746",
-  steel: "B7B7CE",
-  fairy: "D685AD"
-}
+
+
+document.getElementById("details-wrapper").addEventListener("click", function(e){
+    if (e.target === this) {
+      this.style.display = "none";
+      document.getElementById('details-wrapper').style.display = 'none';
+      document.getElementById('loadingScreen').style.display = 'none';
+      document.getElementById('pokemons').style.display = "flex";
+    }
+});
+
+document.getElementById("filter").addEventListener("change", function(){
+  var searchValue = document.getElementById("search").value.toLowerCase();
+  var typeValue = this.value.toLowerCase();
+
+  searchPokemons(typeValue, searchValue);
+});
+
+document.getElementById("search").addEventListener("input", function(){
+  var searchValue = this.value.toLowerCase();
+  var typeValue = document.getElementById("filter").value.toLowerCase();
+
+  searchPokemons(typeValue, searchValue);
+});
